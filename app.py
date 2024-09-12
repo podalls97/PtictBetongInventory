6e96d3ce97703f9e0a725b78a3905f30182c5f1b
unknown <Administrator@PPDQ11NB0060.KPM.local>
2024-09-12 14:52:49 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import psycopg2
+import os
 
 app = Flask(__name__)
 
@@ -149,3 +150,4 @@ def add_device():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
